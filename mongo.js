99432a70f82040375f75d540f5ad22b6f4d6fc18
Pti2mike <mi_ca_elle@hotmail.com>
2024-02-26 15:54:26 +0100

    Find all persons added + improve add person

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length < 5) {
+if (process.argv.length < 3) {
   console.log("give password as argument");
   process.exit(1);
 }
@@ -15,18 +15,37 @@ mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: String || null,
 });
 
 const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
   name: process.argv[3],
-  number: process.argv[4],
+  number: process.argv[4] || null,
 });
 
-person.save().then((result) => {
-  const { name, number } = result;
-  console.log(`added ${name} number ${number} to phonebook`);
-  mongoose.connection.close();
-});
+// Add a person
+if (process.argv.length > 3 && process.argv.length <= 5) {
+  person.save().then((result) => {
+    const { name, number } = result;
+    if (name && number) {
+      console.log(`added ${name} number ${number} to phonebook`);
+    } else if (name) {
+      console.log(`added ${name} without number to phonebook`);
+    }
+    mongoose.connection.close();
+  });
+}
+
+// Find all people
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook: ");
+    result.map((person) => {
+      const { name, number } = person;
+      console.log(name, number);
+    });
+    mongoose.connection.close();
+  });
+}
